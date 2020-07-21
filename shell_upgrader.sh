f10413177eb4fd7f450f40aab0b600d490cbe6ed
Dirox <danielhaal2002@gmail.com>
2020-07-21 15:55:47 +0200

    Add files via upload
    Simplified version of the shell upgrader with execute command function

--- shell_upgrader.sh
+++ shell_upgrader.sh
@@ -1,34 +1,23 @@
 #!/bin/bash
 
+execute(){
+	 xte "str $1"
+	 xte "key Return"
+	 sleep 1
+}
+
 sleep 1
 xte "mouseclick 1"
-xte "str python -c 'import pty; pty.spawn(\"/bin/bash\")'"
-xte "key Return"
-sleep 1
-xte "str python3 -c 'import pty; pty.spawn(\"/bin/bash\")'"
-xte "key Return"
-sleep 1
+execute "python -c 'import pty; pty.spawn(\"/bin/bash\")'"
+execute "python3 -c 'import pty; pty.spawn(\"/bin/bash\")'"
 xte "keydown Control_L" "keydown Z" 
 sleep 1
 xte "keyup Control_L" "keyup Z"
 sleep 1
-xte "str stty raw -echo; fg"
-xte "key Return"
-sleep 1
-xte "str reset"
-xte "key Return"
-sleep 1
-xte "str xterm"
-xte "key Return"
-sleep 1
-xte "str export SHELL=bash"
-xte "key Return"
-sleep 1
-xte "str export TERM=xterm"
-xte "key Return"
-sleep 1
-xte "str stty rows 52 columns 187"
-xte "key Return"
-sleep 1
-xte "str reset"
-xte "key Return"
+execute "stty raw -echo; fg"
+execute "reset"
+execute "xterm"
+execute "export SHELL=bash"
+execute "export TERM=xterm"
+execute "stty rows 52 columns 187"
+execute "reset"
